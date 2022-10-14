26931c6c6e5910e0fbcd80fe4e4754ea945be2eb
faceless49 <digital.zeus@mail.ru>
2022-10-14 20:02:59 +0300

    fix: update

--- src/routes/videosRouter.ts
+++ src/routes/videosRouter.ts
@@ -50,7 +50,11 @@ videosRouter.post('/', [body('title').trim().not().isEmpty().isLength({
     return;
   }
 
-  res.status(400).send({errorMessages: error.array({onlyFirstError: true})})
+  const errors = {
+    errorsMessages: error.array({onlyFirstError: true})
+  }
+
+  res.status(400).send(errors)
 })
 
 
@@ -63,68 +67,66 @@ videosRouter.get('/:id', (req: Request, res: Response) => {
   return res.sendStatus(404)
 })
 
-videosRouter.put<VideoType>('/:id', [body('title').trim().not().isEmpty().isLength({
-  min: 0,
-  max: 40
-}),
-  body('author').not().isEmpty().trim().isLength({
-    min: 0,
-    max: 20,
-  }),
-  body('minAgeRestriction').not().isEmpty().trim().isLength({
-    min: 1,
-    max: 18,
-  }),
-  body('title').trim().not().isEmpty().isLength({
-    min: 0,
-    max: 40
-  }), body('author').not().isEmpty().trim().isLength({
-    min: 0,
-    max: 20,
-  })], (req: Request<VideoType>, res: Response) => {
-  const {
-    id
-  } = req.params;
-  const {
-    author,
-    availableResolutions,
-    minAgeRestriction,
-    publicationDate,
-    title,
-    canBeDownloaded,
-    createdAt
-  } = req.body
-  let video = videos.find((item) => item.id === +id)
+videosRouter.put<VideoType>('/:id',
 
-  if (video) {
-    video = {
-      ...video,
-      id,
-      title,
+  [body('title').trim().not().isEmpty().isLength({
+    min: 1,
+    max: 40
+  }),
+    body('author').not().isEmpty().trim().isLength({
+      min: 1,
+      max: 20,
+    }),
+    body('minAgeRestriction').not().isEmpty().trim().isLength({
+      min: 1,
+      max: 18,
+    })], (req: Request<VideoType>, res: Response) => {
+    const {
+      id
+    } = req.params;
+    const {
       author,
       availableResolutions,
       minAgeRestriction,
       publicationDate,
+      title,
       canBeDownloaded,
       createdAt
-    }
+    } = req.body
+    const video = videos.find((item) => item.id === +id)
+
+    if (video) {
+      const updatedVideo = {
+        ...video,
+        title,
+        author,
+        availableResolutions,
+        minAgeRestriction,
+        publicationDate,
+        canBeDownloaded,
+        createdAt
+      }
+      const index = videos.indexOf(video);
+      if (index > 0) {
+        videos.splice(index, 1, updatedVideo)
+        return res.send(204);
+      }
 
 
-    return res.send(204);
-  }
-  const error = validationResult(req).formatWith(({param, msg,}) => {
-    return {
-      message: msg,
-      field: param
     }
+    const error = validationResult(req).formatWith(({param, msg,}) => {
+      return {
+        message: msg,
+        field: param
+      }
+    })
+    const hasError = !error.isEmpty();
+    if (!video && !hasError) {
+      return res.send(404);
+    }
+
+    return res.send({errorMessages: error.array({onlyFirstError: true})})
   })
-  const hasError = !error.isEmpty();
-  if (!video && !hasError) {
-    return res.send(404);
-  }
-
-  return res.send({errorMessages: error.array({onlyFirstError: true})})
-})
 
 
 videosRouter.delete('/:id', (req: Request, res: Response) => {
